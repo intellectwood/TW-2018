5912d1c94301e7963b4f91d6703c4439e54755c7
bules <125127827@qq.com>
2017-07-31 16:47:42 +0800

    07-30

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,11 +2,11 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
-  var result = [];
-  for(let i = 0; i < collection.length; i++){
-      if(collection[i] % 2 == 0) result.push(collection[i]);
-  }
-  return result;
+    let result = collection.filter((element) => {
+	if(element % 2 == 0) return true;
+	return false;	
+    });
+    return result;
 }
 
 module.exports = collect_all_even;
